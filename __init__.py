0a48bbad6200c4b9a89a7df013e3de3c638e859d
Jake Onquit <148852640+sunrayyourdev@users.noreply.github.com>
2024-08-22 15:13:00 +0800

    feature: shelve persistent storage

--- __init__.py
+++ __init__.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template
-from models.Card import *
-
+from models.Card import Card
+import shelve
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    # insert code here
     return render_template('home.html')
 
 
+# TODO: Creates routes for card CRUD
+
+
 if __name__ == '__main__':
+    with shelve.open('flashcard.db', 'c') as flashcard_database:
+        if not flashcard_database:
+            flashcard_database["Decks"] = {}  # temp database to test card routes
+            # flashcard_database["Users"] = {}
     app.run(debug=True)
