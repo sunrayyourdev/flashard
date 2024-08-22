134a6051c5457b84d3a5bfea77ddff1bd7af41d3
Jake Onquit <148852640+sunrayyourdev@users.noreply.github.com>
2024-08-22 15:22:31 +0800

    refactor: card model and tests - changed to using property decorators for methods - disabled rating feature - refactored tests accordingly

--- models/Card.py
+++ models/Card.py
@@ -1,31 +1,39 @@
 class Card:
+    """A card class which stores information for the user to study"""
 
     def __init__(self, card_id, front, back):
         self.__card_id = card_id
         self.__front = front
         self.__back = back
-        self.__rating = 0
+        # self.__rating = 0
 
-    def get_card_id(self):
+    @property
+    def card_id(self):
         return self.__card_id
     
-    def get_front(self):
+    @property
+    def front(self):
         return self.__front
     
-    def set_front(self, front):
+    @front.setter
+    def front(self, front):
         self.__front = front
     
-    def get_back(self):
+    @property
+    def back(self):
         return self.__back
     
-    def set_back(self, back):
+    @back.setter
+    def back(self, back):
         self.__back = back
     
-    def get_rating(self):
-        return self.__rating
+    # @property
+    # def rating(self):
+    #     return self.__rating
     
-    def set_rating(self, rating):
-        if rating >= 0 and rating <= 4: 
-            self.__rating = rating
-        else:
-            raise ValueError("Invalid rating, must fall between 0 and 4 inclusive")
+    # @rating.setter
+    # def rating(self, rating):
+    #     if rating >= 0 and rating <= 4: 
+    #         self.__rating = rating
+    #     else:
+    #         raise ValueError("Invalid rating, must fall between 0 and 4 inclusive")
--- unit_tests/TestCard.py
+++ unit_tests/TestCard.py
@@ -1,10 +1,9 @@
-
 import sys
 import os
-from unittest import TestCase, main
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
-
+from unittest import TestCase, main
 from models.Card import Card
+
 
 class TestCard(TestCase):
 
@@ -12,33 +11,33 @@ class TestCard(TestCase):
         self.card = Card(0, "Front Text", "Back Text")
 
     def test_get_front(self):
-        self.assertEqual(self.card.get_front(), "Front Text")
+        self.assertEqual(self.card.front, "Front Text")
 
     def test_set_front(self):
-        self.card.set_front("New Front Text")
-        self.assertEqual(self.card.get_front(), "New Front Text")
+        self.card.front = "New Front Text"
+        self.assertEqual(self.card.front, "New Front Text")
 
     def test_get_back(self):
-        self.assertEqual(self.card.get_back(), "Back Text")
+        self.assertEqual(self.card.back, "Back Text")
 
     def test_set_back(self):
-        self.card.set_back("New Back Text")
-        self.assertEqual(self.card.get_back(), "New Back Text")
+        self.card.back = "New Back Text"
+        self.assertEqual(self.card.back, "New Back Text")
 
-    def test_get_rating(self):
-        self.assertEqual(self.card.get_rating(), 0)
+    # def test_get_rating(self):
+    #     self.assertEqual(self.card.rating, 0)
 
-    def test_set_rating_valid(self):
-        self.card.set_rating(3)
-        self.assertEqual(self.card.get_rating(), 3)
+    # def test_set_rating_valid(self):
+    #     self.card.rating = 3
+    #     self.assertEqual(self.card.rating, 3)
 
-    def test_set_rating_invalid(self):
-        with self.assertRaises(ValueError):
-            self.card.set_rating(5)
-        self.assertEqual(self.card.get_rating(), 0)  # Rating should not change
-        with self.assertRaises(ValueError):
-            self.card.set_rating(-1)
-        self.assertEqual(self.card.get_rating(), 0)  # Rating should not change
+    # def test_set_rating_invalid(self):
+    #     with self.assertRaises(ValueError):
+    #         self.card.rating = 5
+    #     self.assertEqual(self.card.rating, 0)  # Rating should not change
+    #     with self.assertRaises(ValueError):
+    #         self.card.rating = -1
+    #     self.assertEqual(self.card.rating, 0)  # Rating should not change
 
 
 if __name__ == "__main__":
